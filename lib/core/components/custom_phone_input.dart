f9f75e219bda7e6786951435fdc38b1e7417bbdf
taskinnasif144 <taskinnasif144@gmail.com>
2025-03-21 15:03:15 +0600

    add custom phone input field component; implement phone number formatting and styling

---
+++ lib/core/components/custom_phone_input.dart
@@ -0,0 +1,47 @@
+import 'package:car_pooling/core/constant/app_colors.dart';
+import 'package:car_pooling/core/constant/app_style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+
+class CustomPhoneInputField extends StatelessWidget {
+  const CustomPhoneInputField({
+    super.key,
+    required this.controller,
+    this.title,
+  });
+  final TextEditingController controller;
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (title != null) Text(title!, style: AppStyle.baseSmallRegular),
+        if (title != null) SizedBox(height: 8.h),
+        Container(
+          // Container to style the field
+          decoration: BoxDecoration(
+            border: Border.all(color: AppColors.primaryLight),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: IntlPhoneField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.zero,
+              counterText: '', // This removes the number length count
+            ),
+            keyboardType: TextInputType.number,
+            initialCountryCode: 'TR', // Contry code for Turkey
+            onChanged: (phone) {
+              // saving the phone number in the controller
+              controller.text = phone.completeNumber;
+            },
+            textAlignVertical: TextAlignVertical.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
