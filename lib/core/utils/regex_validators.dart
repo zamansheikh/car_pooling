5f72e9ca7e6e7e7c33bc3c6884b0247fc9c7203b
taskinnasif144 <taskinnasif144@gmail.com>
2025-03-21 10:56:06 +0600

    add RegexValidators class for email validation and other validators in the future utility

---
+++ lib/core/utils/regex_validators.dart
@@ -0,0 +1,8 @@
+class RegexValidators {
+  static bool isValidEmail(String input) {
+    final emailRegex = RegExp(
+      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+    );
+    return emailRegex.hasMatch(input);
+  }
+}
