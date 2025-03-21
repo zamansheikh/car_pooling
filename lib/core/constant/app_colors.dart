eb08f493569911c1e7a1c76639351828412e079e
taskinnasif144 <taskinnasif144@gmail.com>
2025-03-21 10:25:13 +0600

    update dependencies

---
+++ lib/core/constant/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static Color primary = Color(0xFF66B2A3);
+  static Color primaryDark = Color(0xFF3B8677);
+
+  static Color white = Color(0xFFFCFCFC);
+  static Color primaryLight = Color(0xFFE1E1E1);
+  static Color lightGray = Color(0xFFE0E0E0);
+
+  static Color danger = Color(0xFFF53838);
+
+  static Color dark = Color(0xFFF53838);
+
+  static Color gray = Color(0xFF707070);
+  static Color darkGray = Color(0xFF757575);
+}
