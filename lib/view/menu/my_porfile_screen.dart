6f9c834b2b56fb77d20a3171a951afb66d0f8703
taskinnasif144 <taskinnasif144@gmail.com>
2025-03-27 07:45:29 +0600

    remove MyCarpoolsScreen and MyContactListScreen; add MyPorfileScreen

---
+++ lib/view/menu/my_porfile_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyPorfileScreen extends StatelessWidget {
+  const MyPorfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
