99256a0905cd84aa80830994fb20772d86f87f53
taskinnasif144 <taskinnasif144@gmail.com>
2025-03-27 07:44:39 +0600

    add MenuProfileController to AppBindings for menu functionality

--- lib/core/utils/app_binding.dart
+++ lib/core/utils/app_binding.dart
@@ -2,6 +2,7 @@ import 'package:car_pooling/controller/auth_controller.dart';
 import 'package:car_pooling/controller/carpooling_controller.dart';
 import 'package:car_pooling/controller/find_family_controller.dart';
 import 'package:car_pooling/controller/inbox_controller.dart';
+import 'package:car_pooling/controller/menu_controller.dart';
 import 'package:car_pooling/controller/schedule_controller.dart';
 import 'package:get/get.dart';
 
@@ -12,5 +13,6 @@ class AppBindings {
     Get.lazyPut(() => FindFamilyController(), fenix: true);
     Get.lazyPut(() => ScheduleController(), fenix: true);
     Get.lazyPut(() => InboxController(), fenix: true);
+    Get.lazyPut(() => MenuProfileController(), fenix: true);
   });
 }
